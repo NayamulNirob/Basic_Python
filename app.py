2280969d6d02574f68b0b157d5b5ade31ca14e11
NayamulNirob <nayamulislam@gmail.com>
2025-05-18 19:50:32 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-temprature = int(input("Enter the temperature: "))
-print("Temperature is:", temprature)
-if temprature > 30:
+temperature = int(input("Enter the temperature: "))
+print("Temperature is:", temperature)
+if temperature > 30:
     print("It's hot")
     print("Stay hydrated")
-elif temprature < 20:
+elif temperature < 20:
     print("It's cold")
     print("Wear a jacket")
-elif temprature == 30:
+elif temperature == 30:
     print("It's a warm day")
     print("Perfect for a walk")
+print("Have a great day!")
